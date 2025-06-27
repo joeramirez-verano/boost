9fd41d7fe06a484174c6aca3047f7fb51f3d4b73
Muhammad Ahmad Faraz <ahmadfaraz629@gmail.com>
2025-06-27 15:05:32 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+    base: '/boost/',
     // Add your Vite configuration options here
     server: {
         port: 3000,
