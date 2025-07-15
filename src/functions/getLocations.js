a13543a56e84e1ad51cee906fa1390efb9e812ef
Ahmad Faraz <ahmadfaraz629@gmail.com>
2025-07-15 22:06:36 +0500

    feat: Enhance location fetching functions to include fallback to zenleafdispensaries.com for improved reliability

--- src/functions/getLocations.js
+++ src/functions/getLocations.js
@@ -2,7 +2,15 @@
  *
  */
 export default async function getLocations() {
-  const response = await fetch(`${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api/locations/?exclude_keys=false`)
-  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-  return await response.json()
+  let response;
+  try {
+    response = await fetch(`${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api/locations/?exclude_keys=false`)
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+    return await response.json()
+  } catch (e) {
+    // fallback to zenleafdispensaries.com
+    response = await fetch(`https://zenleafdispensaries.com/api/locations/?exclude_keys=false`)
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+    return await response.json()
+  }
 }
--- src/functions/getStateBySlug.js
+++ src/functions/getStateBySlug.js
@@ -8,8 +8,15 @@ export default async function getStateBySlug(slug) {
   if (!slug) {
     return {}
   }
-
-  const response = await fetch(`${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api/locations/${slug}/`)
-  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-  return await response.json()
+  let response;
+  try {
+    response = await fetch(`${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api/locations/${slug}/`)
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+    return await response.json()
+  } catch (e) {
+    // fallback to zenleafdispensaries.com
+    response = await fetch(`https://zenleafdispensaries.com/api/locations/${slug}/`)
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+    return await response.json()
+  }
 }
